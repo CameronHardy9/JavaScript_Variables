a4d6d272a9cc0a57d9bb20cc46bce27d4d044346
Cameron Hardy <camhardy99@gmail.com>
2021-08-25 10:32:52 -0500

    updated app.js with exercise info

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Cameron";
+let lastName = "Hardy";
+var age = 29;
 
 // Exercise 2
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
 
 // Exercise 3
+let city = "Birmingham";
+let state = "Alabama";
+const pastTime = "Coffee";
+let myStory = `Hello! My name is ${fullName}. I live in ${city}, ${state}. I enjoy ${passtime} and coding!`;
+console.log(myStory);
