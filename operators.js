10b71e6763ffc14602ff38e4bd9a84285d2c75c3
Cameron Hardy <camhardy99@gmail.com>
2021-08-25 13:50:47 -0500

    added operators.js for operators exercises 1-3

---
+++ operators.js
@@ -0,0 +1,49 @@
+// JS Operators Exercise
+
+//Exercise 1
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(`${add}\n${minus}\n${multiply}\n${dividing}`)
+
+
+//Exercise 2
+let a = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+/*
+What is the value of: a + str?  ANSWER = "1111"
+What is the value of: a + str2? ANSWER = "11eleven"
+What is the value of: a + isPresent? ANSWER = 12
+What is the value of: a + firstName? ANSWER = "11Jackie"
+What is the value of: a + lastName? ANSWER = "11Chan"
+*/
+
+
+//Exercise 3
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+/*
+What is the value of: d == str3?  ANSWER = true
+What is the value of: d === str3?  ANSWER = false
+What is the value of: !isPresent2?  ANSWER = true
+What is the value of: (“eleven” == str4 && d >= str3)?  ANSWER = false
+What is the value of: (!isPresent2 || isPresent2)?  ANSWER = true
+What is the value of: 0 == false?  ANSWER = true
+What is the value of: 0 === false?  ANSWER = false
+What is the value of: 0 != false?  ANSWER = false
+What is the value of 0 !== false?  ANSWER = true
+*/
